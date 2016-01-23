8085340e823ce87c2ce4e6a99fcfabf06b5e98dc
Dominique DERRIER <derrierdo@gmail.com>
2016-01-23 08:56:10 -0500

    Download with day/night

--- download.py
+++ download.py
@@ -12,16 +12,19 @@ except FileExistsError:
     print("contiune anyway")
 
 os.chdir("screensavers")
+os.mkdir("night")
+os.mkdir("day")
 
 
 url = "http://a1.phobos.apple.com/us/r1000/000/Features/atv/AutumnResources/videos/entries.json"
 
-data= json.loads(request.urlopen(url).read().decode('utf-8'))
+data = json.loads(request.urlopen(url).read().decode('utf-8'))
 
 for a in data:
     for b in a['assets']:
-        url=b['url']
-        name="%s-%s.mov"%(b['accessibilityLabel'],b['id'])
-        print("Download %s --> %s"%(url,name))
-        request.urlretrieve(url,name)
-
+        url = b['url']
+        name = "%s/%s-%s.mov" % (b['timeOfDay'],
+                                 b['accessibilityLabel'],
+                                 b['id'])
+        print("Download %s --> %s" % (url, name))
+        request.urlretrieve(url, name)
